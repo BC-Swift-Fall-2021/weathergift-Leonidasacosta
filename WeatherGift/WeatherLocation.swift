826f3e274f5167ec7fb604c9c0d73dfe6983ed75
Leonidasacosta <89566738+Leonidasacosta@users.noreply.github.com>
2021-10-05 19:54:11 -0400

    Completed Video 6.2: WeatherLocations List Table View!

---
+++ WeatherGift/WeatherLocation.swift
@@ -0,0 +1,14 @@
+//
+//  WeatherLocation.swift
+//  WeatherGift
+//
+//  Created by Leonidas Acosta on 10/5/21.
+//
+
+import Foundation
+
+struct WeatherLocation {
+    var name: String
+    var latitude: Double
+    var longitude: Double
+}
