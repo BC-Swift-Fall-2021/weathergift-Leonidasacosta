679dc6fa9901637de02d7b55af440776f25cc6d4
Leonidasacosta <89566738+Leonidasacosta@users.noreply.github.com>
2021-10-21 18:29:14 -0400

    Completed Video 6.16! Getting Hourly Weather Data from the OpenWeather API!

--- WeatherGift/WeatherDetail.swift
+++ WeatherGift/WeatherDetail.swift
@@ -8,10 +8,15 @@
 import Foundation
 
 private let dateFormatter: DateFormatter = {
-    print("📆📆📆 I JUST CREATED A DATE FORMATTER in WeatherDetail.swift!")
     let dateFormatter = DateFormatter()
     dateFormatter.dateFormat = "EEEE"
     return dateFormatter
+}()
+
+private let hourFormatter: DateFormatter = {
+    let hourFormatter = DateFormatter()
+    hourFormatter.dateFormat = "ha"
+    return hourFormatter
 }()
 
 struct DailyWeather {
@@ -21,6 +26,13 @@ struct DailyWeather {
     var dailyHigh: Int
     var dailyLow: Int
 }
+struct HourlyWeather {
+    var hour: String
+    var hourlyTemperature: Int
+    var hourlyIcon: String
+}
+
+
 
 class WeatherDetail: WeatherLocation {
     
@@ -28,6 +40,7 @@ class WeatherDetail: WeatherLocation {
         var timezone: String!
         var current: Current
         var daily: [Daily]
+        var hourly: [Hourly]
     }
     
     private struct Current: Codable {
@@ -47,11 +60,18 @@ class WeatherDetail: WeatherLocation {
         var weather: [Weather]
     }
     
+    private struct Hourly: Codable {
+        var dt: TimeInterval
+        var temp: Double
+        var weather: [Weather]
+    }
     
     private struct Temp: Codable {
         var max: Double
         var min: Double
     }
+    
+
     
     var timezone = ""
     var currentTime = 0.0
@@ -59,6 +79,7 @@ class WeatherDetail: WeatherLocation {
     var summary = ""
     var dayIcon = ""
     var dailyWeatherData: [DailyWeather] = []
+    var hourlyWeatherData: [HourlyWeather] = []
     
     func getData(completed: @escaping () -> ()) {
         let urlString = "https://api.openweathermap.org/data/2.5/onecall?lat=\(latitude)&lon=\(longitude)&exclude=minutely&units=imperial&appid=\(APIkeys.openWeatherKey)"
@@ -103,6 +124,20 @@ class WeatherDetail: WeatherLocation {
                     let dailyWeather = DailyWeather(dailyIcon: dailyIcon, dailyWeekday: dailyWeekday, dailySummary: dailySummary, dailyHigh: dailyHigh, dailyLow: dailyLow)
                     self.dailyWeatherData.append(dailyWeather)
                     print("Day: \(dailyWeekday), High: \(dailyHigh), Low: \(dailyLow)")
+                }
+                // get no more than 24 hrs of hourly data
+                let lastHour = min(24, result.hourly.count)
+                if lastHour > 0 {
+                    for index in 1...lastHour {
+                        let hourlyDate = Date(timeIntervalSince1970: result.hourly[index].dt)
+                        hourFormatter.timeZone = TimeZone(identifier: result.timezone)
+                        let hour = hourFormatter.string(from: hourlyDate)
+                        let hourlyIcon = self.fileNameForIcon(icon: result.hourly[index].weather[0].icon)
+                        let hourlyTemperature = Int(result.hourly[index].temp.rounded())
+                        let hourlyWeather = HourlyWeather(hour: hour, hourlyTemperature: hourlyTemperature, hourlyIcon: hourlyIcon)
+                        self.hourlyWeatherData.append(hourlyWeather)
+                        print("Hour: \(hour), Temperature: \(hourlyTemperature), Icon: \(hourlyIcon)")
+                    }
                 }
             } catch {
                 print("😡 JSON ERROR: \(error.localizedDescription)")
